837e96ca2a4ce85899684f84379530ddceec7adc
Carson Hall <carsonh@gmail.com>
2022-10-17 17:28:47 -0400

    Import component error Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
@@ -5,8 +6,22 @@ const Navbar = () => {
   return (
     // Use Navbar.module.css for all styles
 
-    <div className={styles.container}>Navbar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+    {/* // phone icon */}
+    <div className={styles.callButton}>
+    <Image src="/img/purplescheme.JPG" alt="phone" width="32" height="32" />
+    </div>
+    // menu links
+    <div className={styles.texts}>
+      <div className={styles.text}>PLACE ORDER</div>
+      <div className={styles.text}>502 555 5961</div>
+</div>
+</div>
+    <div className={styles.item}>Navbar</div>
+    <div className={styles.item}>Navbar</div>
+    </div>
+  );
+};
 
 export default Navbar
