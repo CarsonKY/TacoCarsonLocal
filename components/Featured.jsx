e770bc03e7ef0acbb7d5af9eb44ee143cdff0551
Carson <carsonh@gmail.com>
2022-10-23 12:03:00 -0400

    Begin Featured Styling

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css"
 
+// Create Array to load Featured images 
 const Featured = () => {
     const images = [
         "/img/featured.png",
@@ -9,19 +10,26 @@ const Featured = () => {
         "/img/featured3.png",
     ];
   return (
+      // Create divs and load container wrapper and imgC styles from CSS
     <div className={styles.container}>
-    <Image src="/img/arrowl.png" width="512" height="512" alt="" />
+        <div className={styles.arrowContainer}>
+    <Image src="/img/arrowl.png" alt="" layout='fill' />
+    </div>
     <div className={styles.wrapper}>
     <div className={styles.imgContainer}>
         {images.map((img, i)  => (
-            <Image src="/img/featured.png" width="512" height="512" key={i} alt="" />
-            
+            // images require width and height, workaround is layout fill for 100% width
+            // No individual ids for images required so using index number for each child for the key
 
+            <Image src="/img/featured.png" key={i} alt="" layout='fill'/>
+            
         ))}
     
     </div>
     </div>
+    <div className={styles.arrowContainer}>
     <Image src="/img/arrowr.png" width="512" height="512"alt="" />
+    </div>
     </div>
   )
 }
