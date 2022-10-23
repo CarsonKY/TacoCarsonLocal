8207a1d6459fad6e811bf5b33327900e7ced55f7
Carson <carsonh@gmail.com>
2022-10-23 10:04:03 -0400

    Unexpected token errors

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -3,8 +3,25 @@ import Image from "next/image";
 import styles from "../styles/Navbar.module.css"
 
 const Featured = () => {
+    const images = [
+        "/img/featured.png"
+        "/img/featured2.png"
+        "/img/featured3.png"
+    ];
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+    <Image src="/img/arrowl.png" alt="" />
+    <div className={styles.wrapper}>
+    <div className={styles.imgContainer}>
+        {images.map(img)  => (
+            (Img/featured.png" alt="" />
+
+        ))}
+    <Image src="/img/featured.png" alt="" />
+    </div>
+    </div>
+    <Image src="/img/arrowr.png" alt="" />
+    </div>
   )
 }
 
