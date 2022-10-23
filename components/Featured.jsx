20f96c8e950e73dc845ccb4215da6db2f47c3f96
Carson <carsonh@gmail.com>
2022-10-23 15:28:30 -0400

    Featured pic importing not displaying right

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Featured.module.css"
 
 // Create Array to load Featured images 
+// Project requirement
 const Featured = () => {
     const images = [
         "/img/featured.png",
@@ -10,19 +11,18 @@ const Featured = () => {
         "/img/featured3.png",
     ];
   return (
+    // images require width and height, workaround is layout fill for 100% width
       // Create divs and load container wrapper and imgC styles from CSS
     <div className={styles.container}>
+     
         <div className={styles.arrowContainer} style={{left:0}}>
     <Image src="/img/arrowl.png" alt="" layout='fill' />
     </div>
     <div className={styles.wrapper}>
-    
+            {/* No individual ids for images required so using index number for each child for the key */}
         {images.map((img, i)  => (
             <div className={styles.imgContainer} key={i}>
-            {/* // images require width and height, workaround is layout fill for 100% width
-            // No individual ids for images required so using index number for each child for the key */}
-
-            <Image src={img}  alt="" layout='fill'/>
+             <Image src={img}  alt="" layout='fill'/>
             </div>
             
         ))}
