01225d2f2195294f534a275fd3072997d0040267
Carson <carsonh@gmail.com>
2022-10-23 23:33:48 -0400

    No default export error

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -5,18 +5,29 @@ import styles from "../styles/Featured.module.css"
 // Create Array to load Featured images 
 // Project requirement
 const Featured = () => {
+    // Usestate hook to rotate images starting with first image
+    const [index, setIndex] = useState(0)
     const images = [
         "/img/featured.png",
         "/img/featured2.png",
         "/img/featured3.png",
     ];
+// Conditional rendering when arrow is pressed for picture scrolling
+    const handleArrow = (direction) =>{
+        if(direction==="l"){
+            setIndex(index !== 0 ? index-1 : 2)
+        }
+            if(direction==="r"){
+                setIndex(index !== 2 ? index+1 : 0
+        )
+    }
   return (
     // images require width and height, workaround is layout fill for 100% width
       // Create divs and load container wrapper and imgC styles from CSS
     <div className={styles.container}>
      
-        <div className={styles.arrowContainer} style={{left:0}}>
-    <Image src="/img/arrowl.png" alt="" layout='fill' />
+        <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow("l")}>
+    <Image src="/img/arrowl.png" alt="" layout='fill' objectFit="contain"/>
     </div>
     <div className={styles.wrapper}>
             {/* No individual ids for images required so using index number for each child for the key */}
@@ -29,7 +40,7 @@ const Featured = () => {
     
     
     </div>
-    <div className={styles.arrowContainer} style={{right:0}}>
+    <div className={styles.arrowContainer} style={{right:0}} onClick={()=>handleArrow("r")}>
     <Image src="/img/arrowr.png" alt="" layout='fill'/>
     </div>
     </div>
