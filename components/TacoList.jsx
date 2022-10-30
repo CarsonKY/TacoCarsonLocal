d0fcbf725f6be68b82fb0ab1856358aba9f1c691
Carson <carsonh@gmail.com>
2022-10-30 02:41:28 -0400

    Welcome to TC image work

--- components/TacoList.jsx
+++ components/TacoList.jsx
@@ -1,9 +1,13 @@
 import styles from "../styles/TacoList.module.css"
 import TacoCard from "./TacoCard";
+// @ts-nocheck
+import Image from "next/image";
+
 
 const TacoList = () => {
   return (
     <div className={styles.container}>
+        <Image src="/img/tacocarsonwelcomesyou.PNG" alt="tacocarsonwelcomesyou" width="500px" height="150px" />
       <h1 className={styles.title}>TACO CARSON WELCOMES YOU</h1>
       <p className={styles.desc}>
           Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! Best Tacos!  Come on In! 
