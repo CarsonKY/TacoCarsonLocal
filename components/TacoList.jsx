46576c0908ff1a5ce171d64d84391e26738abf3b
Carson <carsonh@gmail.com>
2022-10-30 00:24:04 -0400

    Beginning of menu component

---
+++ components/TacoList.jsx
@@ -0,0 +1,14 @@
+import styles from "../styles/TacoList.module.css"
+
+const TacoList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>TACO CARSON WELCOMES YOU</h1>
+      <p className={styles.desc}>
+          Best Tacos!  Come on In!
+      </p>
+      </div>
+  );
+};
+
+export default TacoList
