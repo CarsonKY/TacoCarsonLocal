64b8048f9285c6c27e3c92624ead187049791f8b
Carson Hall <carsonh@gmail.com>
2022-11-01 16:08:29 -0400

    Continued Products Styling

--- pages/api/product/[id].jsx
+++ pages/api/product/[id].jsx
@@ -1,27 +1,41 @@
 // @ts-nocheck
 import styles from "../styles/Product.module.css"
 import Image from "next/image";
-import TacoList from "../../../components/TacoList";
+
 
 
 
 const Product = () => {
     const taco = {
         id: 1,
-        img: "/img/beeftacos.small.jpg",
+        img: "/img/arrowl.jpg",
         name: "BEEF TACOS",
         price: [3.50],
         desc:"Beef Tacos", 
     
     };
-  return <div className={styles.container}>;
-  <div className={styles.left}>;
+  return <div className={styles.container}>
+  <div className={styles.left}>
   <div className={styles.imgContainer}>
-    <Image src={tacos.img} layout="fill" alt=""/>
+    <Image src={taco.img} layout="fill" objectFit="contain" alt=""/>
 
   </div>
     </div>
   <div className={styles.right}></div>;
+    <h1 className={styles.title}>{taco.name}</h1>
+    <span className={styles.price}>"${taco.price}</span>
+    <p className={styles.desc}>{taco.desc}</p>
+    <h3 className={styles.choose}>Extra beef or cheese?</h3>
+    <div className={styles.ingredients}>
+    <div className={styles.option}>
+        <input 
+        type="checkbox" 
+        id="extracheese" 
+        name="extracheese" 
+        className={styles.checkbox} />
+
+    </div>
+    </div>
     </div>
  
  
