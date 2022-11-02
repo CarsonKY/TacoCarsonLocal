912027c4cdee64816766283f219c4187b694e56e
Carson Hall <carsonh@gmail.com>
2022-11-02 13:42:09 -0400

    Continued Cart module work

--- pages/api/product/Cart.jsx
+++ pages/api/product/Cart.jsx
@@ -7,7 +7,42 @@ const Cart = () => {
     <div className={styles.container}>
         <div className={styles.left}>
             <table className={styles.table}>
-              <tr className={styles.tr}
+              <tr className={styles.tr}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              </tr>
+              <tr>
+                    <td>
+                        <div className={styles.imgContainer}>
+                            <Image src="/img/beeftacos.large.jpg" 
+                            layout="fill" 
+                            objectFit="cover" 
+                            alt=""></Image>
+                        </div>
+                    </td>
+                    <td>
+                        <span className={styles.name}>BEEF TACOS</span>
+                    </td>
+                    <td>
+                        <span className={styles.extras}>
+                            Double meat, Extra Cheese
+                        </span>
+                    </td>
+                    <td>
+                        <span className={styles.price}>$3.50</span>
+                    </td>
+                    <td>
+                        <span className={styles.quantity}>2</span>
+                    </td>
+                    <td>
+                        <span className={styles.total}>$7.00</span>
+                    </td>
+              </tr>
+              </table>
 
         </div>
         <div className={styles.right}></div>
