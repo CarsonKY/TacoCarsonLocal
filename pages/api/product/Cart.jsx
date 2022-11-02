b1620fe146fc737e3684a826dc4b0936178c93a0
Carson Hall <carsonh@gmail.com>
2022-11-02 12:48:55 -0400

    Cart module beginning work

---
+++ pages/api/product/Cart.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/Cart.module.css"
+// @ts-ignore
+import Image from "next/image";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+            <table className={styles.table}>
+              <tr className={styles.tr}
+
+        </div>
+        <div className={styles.right}></div>
+
+
+    </div>
+  )
+}
+
+export default Cart
