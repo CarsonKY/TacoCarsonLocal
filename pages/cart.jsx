3eff17bacde6b8cf563746819007c3ee2cf6a445
Carson <carsonh@gmail.com>
2022-11-13 12:41:00 -0500

    Moving from local array to mongo in cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,6 +6,7 @@ import {useDispatch, useSelector } from "react-redux"
 // move everything from store to cart
 const Cart = () => {
     const dispatch = useDispatch();
+    const cart = useSelector(state=>state.cart)
     
   return (
     <div className={styles.container}>
@@ -19,33 +20,38 @@ const Cart = () => {
               <th>Quantity</th>
               <th>Total</th>
               </tr>
-              <tr>
+              {cart.products.map(product=>(
+              <tr className={styles.tr} key={product.id}>
                     <td>
                         <div className={styles.imgContainer}>
-                            <Image src="/menu/beeftacos.large.jpg" 
+                            <Image src={product.img} 
                             layout="fill" 
                             objectFit="cover" 
                             alt=""></Image>
                         </div>
                     </td>
                     <td>
-                        <span className={styles.name}>BEEF TACOS</span>
+                        <span className={styles.name}>{product.title}</span>
                     </td>
                     <td>
                         <span className={styles.extras}>
+                            {product.extras.map(extra=>(
+                                <span key={extra._id}>{extra.text},</span>
+                            ))}
                             Double meat, Extra Cheese
                         </span>
                     </td>
                     <td>
-                        <span className={styles.price}>$3.50</span>
+                        <span className={styles.price}>{product.price}</span>
                     </td>
                     <td>
-                        <span className={styles.quantity}>2</span>
+                        <span className={styles.quantity}>{product.quantity}</span>
                     </td>
                     <td>
-                        <span className={styles.total}>$7.00</span>
+                        <span className={styles.total}>${product.price * product.quantity}</span>
                     </td>
               </tr>
+              ))}
               </table>
 
         </div>
@@ -53,13 +59,13 @@ const Cart = () => {
         <div className={styles.wrapper}>
             <h2 className={styles.title}>CART TOTAL</h2>
             <div className={styles.totalText}></div>
-                <b className={styles.totalTextTitle}>Subtotal:</b>$7.00
+                <b className={styles.totalTextTitle}>Subtotal:</b>${cart.total}
                 </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Tax:</b>$0.42
+            <b className={styles.totalTextTitle}>Tax:</b>{cart.total * .06}
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>$7.42
+            <b className={styles.totalTextTitle}>Total:</b>{cart.total * .06 + cart.total}
           </div>
           <button className={styles.button}>CHECKOUT! </button>
         </div>
