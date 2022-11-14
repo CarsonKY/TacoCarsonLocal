d0be79457e70aff81119a28bdfcdb87e97f4c32a
Carson <carsonh@gmail.com>
2022-11-13 22:11:41 -0500

    Cart page redo - still NaN for grand total

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,80 +1,78 @@
-import styles from "../styles/Cart.module.css"
+import styles from "../styles/Cart.module.css";
 // @ts-ignore
 import Image from "next/image";
-import {useDispatch, useSelector } from "react-redux"
-import { useEffect, useState } from "react";
+import { useDispatch, useSelector} from "react-redux";
 
-// move everything from store to cart
+
 const Cart = () => {
-    const dispatch = useDispatch();
-    
-    const cart = useSelector((state) => state.cart);
-    
+    const dispatch = useDispatch()
+    const cart = useSelector((state)=>state.cart);
   return (
     <div className={styles.container}>
-        <div className={styles.left}>
-            <table className={styles.table}>
-              <tr className={styles.tr}>
-              <th>Product</th>
-              <th>Name</th>
-              <th>Extras</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-              </tr>
-              {cart.products.map(product=>(
-              <tr className={styles.tr} key={product.id}>
-                    <td>
-                        <div className={styles.imgContainer}>
-                            <Image src={product.img} 
-                            layout="fill" 
-                            objectFit="cover" 
-                            alt=""></Image>
-                        </div>
-                    </td>
-                    <td>
-                        <span className={styles.name}>{product.title}</span>
-                    </td>
-                    <td>
-                        <span className={styles.extras}>
-                            {product.extras.map(extra=>(
-                                <span key={extra._id}>{extra.text},</span>
-                            ))}
-                            
-                        </span>
-                    </td>
-                    <td>
-                        <span className={styles.price}>{product.prices}</span>
-                    </td>
-                    <td>
-                        <span className={styles.quantity}>{product.quantity}</span>
-                    </td>
-                    <td>
-                        <span className={styles.total}>${product.prices * product.quantity}</span>
-                    </td>
-              </tr>
-              ))}
-              </table>
-
-        </div>
-        <div className={styles.right}></div>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.trTitle}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          {cart.products.map(product=>(
+          <tr className={styles.tr} key={product._id}>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src={product.img}
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>{product.title}</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                {product.extras.map(extra=>(
+                    <span key={extra.id}>{extra.text}</span>
+                ))}
+                
+              </span>
+            </td>
+            <td>
+              <span className={styles.prices}>${product.prices}</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>{product.quantity}</span>
+            </td>
+            <td>
+              <span className={styles.total}>${product.prices * product.quantity}</span>
+            </td>
+          </tr>
+          ))}
+        </table>
+      </div>
+      <div className={styles.right}>
         <div className={styles.wrapper}>
-            <h2 className={styles.title}>CART TOTAL</h2>
-            <div className={styles.totalText}></div>
-                <b className={styles.totalTextTitle}>Subtotal:</b>{cart.total}
-                </div>
+          <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Tax:</b>{cart.total * .06}
+            <b className={styles.totalTextTitle}>Subtotal:</b>
+            {/* {cart.total} */}
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>{cart.total}
-            
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
           </div>
-          <button className={styles.button}>CHECKOUT! </button>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
         </div>
-      
+      </div>
+    </div>
   );
-  
 };
 
 export default Cart;
